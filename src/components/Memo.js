eb8a7cd49f45033dbfea17d27292f7f3cabaecda
alma-hernandez <1984ahern@gmail.com>
2024-02-23 16:08:54 -0600

    used useMemo to sort list

--- src/components/Memo.js
+++ src/components/Memo.js
@@ -1,8 +1,33 @@
-import React, { useMemo } from "react";
+import React, { useState, useMemo } from 'react';
 
 const Memo = () => {
+
+ const [list, setList] = useState([]);
+
+ const addItem = (item) => {
+  setList(prevList => [...prevList, item]);
+ };
+
+
+ const sortedList = useMemo(() => {
+  return [...list].sort((a, b) => a - b);
+ }, [list]);
+
+
+
  return (
- <div>Memo</div>
-)};
+  <div>
+   <h2>Sorted List</h2>
+   <ul>
+    {sortedList.map(item => (
+     <li key={item}>{item}</li>
+    ))}
+   </ul>
+   <button onClick={() => addItem(Math.floor(Math.random() * 100))}>
+    Add Random Item
+   </button>
+  </div>
+ );
+};
 
 export default Memo;
