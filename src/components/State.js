14941d6975f426fb8c96c4591c32743524c38732
alma-hernandez <1984ahern@gmail.com>
2024-02-23 15:04:36 -0600

    use useState

--- src/components/State.js
+++ src/components/State.js
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
 
 const State = () => {
- return (
-  <div>State</div>
- )
+  const [color, setColor] = useState("red");
+
+  const changeColor = () => {
+    setColor(color === "red" ? "blue" : "red");
+  };
+  
+  return (
+    <div>
+      <div>Using useState Example</div>
+      <button style={{ backgroundColor: color }} onClick={changeColor}>Change Color</button>
+    </div>
+  );
 };
 
 export default State;
