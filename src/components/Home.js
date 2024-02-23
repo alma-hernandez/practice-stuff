14d83d28bd91553f11e27980d1caa86e2cce7bd5
alma-hernandez <1984ahern@gmail.com>
2024-02-23 14:44:50 -0600

    created links

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
  return (
+  <div>
  <div>Home</div>
+ <Link to="Effect">Click to see Effect page</Link><br />
+ <Link to="Memo">Click to see Memo page</Link><br />
+ <Link to="State">Click to see State page</Link>
+ </div>
 )};
 
 export default Home;
