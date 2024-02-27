c807676fdf0f7981255afff80a6db60bff6e72f2
David Young <xiaoyang88@gmail.com>
2024-02-27 10:52:25 +0800

    fix context component

--- src/components/HeaderContext.js
+++ src/components/HeaderContext.js
@@ -1,15 +1,29 @@
-import React, { useContext } from "react";
-import { ThemeContext } from "./Context"; 
+import React, { useState, createContext, useContext } from "react";
+const ThemeContext = createContext(null);
+
+const Context = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <HeaderContext/>
+    </ThemeContext.Provider>
+  );
+};
 
 const HeaderContext = () => {
   const { theme, toggleTheme } = useContext(ThemeContext); 
 
   return (
-    <header style={{ background: theme === "light" ? "blue" : "light" }}>
+    <header style={{ background: theme === "light" ? "blue" : "Red" }}>
       <h1>Theme Switching</h1>
       <button onClick={toggleTheme}>Toggle Theme</button>
     </header>
   );
 };
 
-export default HeaderContext;
+export default Context;
